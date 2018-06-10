2e50f616bed24acad00e17ecdd99b4b7155898c1
Marcos-Gonzales <codeforlife86@outlook.com>
2018-06-10 11:30:03 -0700

    updated app.js with game logic

--- js/app.js
+++ js/app.js
@@ -1,26 +1,54 @@
-/*
- * Variables
- */
+/***Variables***/
 const wonMsg = 'You Win! Play again?';
-let restart = document.getElementsByClassName('restart')[0];
-let deck = document.getElementsByTagName('ul')[1];
-let moves = document.getElementsByClassName('moves')[0];
+let restart = document.querySelector('.restart');
+let deck = document.querySelector('.deck');
+let moves = document.querySelector('.moves');
+let counter = 0;
+let hard = 10;
+let medium = 20;
 let totalMoves = moves.innerText;
 let stars = document.getElementsByClassName('fa-star');
+let numStars = 3;
+let openedCards = [];
+let timeTracker;
+let t = document.querySelector('.timer');
 
-/*
- * Create a list that holds all of your cards
- */
-const faClasses = [
-  'fa fa-diamond',
-  'fa fa-paper-plane-o',
-  'fa fa-anchor',
-  'fa fa-bolt',
-  'fa fa-cube',
-  'fa fa-leaf',
-  'fa fa-bicycle',
-  'fa fa-bomb'
-]
+
+/***Create a list that holds all of your cards***/
+const cardClasses = [
+  'fa-diamond',
+  'fa-diamond',
+  'fa-paper-plane-o',
+  'fa-paper-plane-o',
+  'fa-anchor',
+  'fa-anchor',
+  'fa-bolt',
+  'fa-bolt',
+  'fa-cube',
+  'fa-cube',
+  'fa-leaf',
+  'fa-leaf',
+  'fa-bicycle',
+  'fa-bicycle',
+  'fa-bomb',
+  'fa-bomb'
+];
+
+/***Init Game***/
+function startGame() {
+  let cards = shuffle(cardClasses).map(function(c) {
+    return createCard(c);
+  });
+  deck.innerHTML = cards.join('');
+  startTimer();
+}
+
+startGame();
+
+// HTML to create cards dynamically
+function createCard(c) {
+  return `<li class='card'><i class='fa ${c}'></i></li>`;
+}
 
 
 /*
@@ -29,6 +57,54 @@ const faClasses = [
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+function showCard(c) {
+  c.classList.add('open', 'show');
+}
+
+function hideCard(c) {
+  c.classList.remove('open', 'show');
+}
+
+// Hides cards after x(sec)
+function timeOut(c, secs) {
+  setTimeout(function() {
+    openedCards.forEach(function(c) {
+      hideCard(c);
+    });
+    openedCards = [];
+  }, secs);
+}
+
+let totalCards = document.querySelectorAll('.card');
+
+// Goes through each card, adds an event listener, and checks if they have classes 'open show match'
+// If true, add to openedCards[]
+totalCards.forEach(function(c) {
+  c.addEventListener('click', function(e) {
+    if (!c.classList.contains('open') && !c.classList.contains('show') && !c.classList.contains('match')) {
+      openedCards.push(c);
+      showCard(c);
+
+      if (openedCards.length === 2) {
+
+        // Check for equal match. If true - add class 'match'
+        if (openedCards[0].isEqualNode(openedCards[1])) {
+          openedCards[0].classList.add('match');
+          hideCard(c);
+          openedCards[1].classList.add('match');
+          hideCard(c);
+          openedCards = [];
+        } else {
+          // No-match: hide cards after 1sec
+          timeOut(c, 1000);
+        }
+        counter++;
+        moveCounter();
+      }
+    }
+  });
+});
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -46,6 +122,64 @@ function shuffle(array) {
   return array;
 }
 
+/***Timer***/
+let secs = document.getElementById("seconds");
+let mins = document.getElementById("minutes");
+let sec = 0;
+
+function timer(t) {
+  return t > 9 ? t : "0" + t;
+}
+
+function startTimer() {
+  timeTracker = setInterval(function() {
+    secs.innerHTML = timer(++sec % 60);
+    mins.innerHTML = timer(parseInt(sec / 60, 10));
+  }, 1000);
+}
+
+function resetTimer() {
+  secs = 1;
+  mins = 0;
+  startTimer()
+}
+
+// Update move counter
+function moveCounter() {
+  moves.textContent = counter;
+
+  if (counter === hard || counter === medium) {
+    removeStar();
+  }
+}
+
+// Remove a star from rating
+function removeStar() {
+  let lastStar = document.querySelector('.fa-star');
+  lastStar.parentNode.removeChild(lastStar);
+  lastStar.attr('class', 'fa fa-star-o');
+  numStars--;
+  // $(".num-stars").text(String(numStars));
+}
+
+function resetStars() {
+  let starsReset = document.querySelector('.fa-star-o');
+  starsReset.attr('class', 'fa fa-star');
+  numStars = 3;
+  // $(".num-stars").text(String(numStars));
+}
+
+let resetGame = function() {
+  openedCards = [];
+  counter = 0;
+  moveCounter();
+  resetStars();
+  resetTimer();
+  clearInterval(timeTracker);
+  startGame();
+};
+
+restart.addEventListener('click', resetGame);
 
 /*
  * set up the event listener for a card. If a card is clicked:
